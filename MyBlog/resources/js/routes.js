50886a1b420b29abcf94ee12958c97bc16d97f65
AlphaSpirits <alpha.4spirits@gmail.com>
2020-02-19 07:20:44 +0530

    Initial Commit

---
+++ MyBlog/resources/js/routes.js
@@ -0,0 +1,11 @@
+import Signup from './components/Signup';
+
+export const routes = [
+    {
+    path: '/sign-up',
+    name: 'sign-up',
+    meta: {layout:'sign-up'},
+    component: Signup
+}
+];
+
